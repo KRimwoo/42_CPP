59e4be4415e514c56a4dad277586d3c503e0d571
Woorim Kim <woorikim@student.42seoul.kr>
2024-06-10 13:39:48 +0900

    fix ex00

--- CppModule05/ex00/main.cpp
+++ CppModule05/ex00/main.cpp
@@ -3,14 +3,16 @@
 int main()
 {
     try {
+        std::cout << "=====Creating a BureaucratA=====" << std::endl;
         Bureaucrat a("A", 200);
         std::cout << a;
     }
     catch (const std::exception &e) {
         std::cerr << e.what();
     }
-    
+
     try {
+        std::cout << "\n=====Creating a BureaucratB, C, D, E=====" << std::endl;
         Bureaucrat b("B", 150);
         Bureaucrat c = b;
         Bureaucrat d;
